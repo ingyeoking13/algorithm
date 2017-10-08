f408c7aa6976cf5aafb4529e06318399c021a164
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2017-10-08 08:11:29 +0000

    race

--- cf/implementation/p868b.c
+++ cf/implementation/p868b.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
 
-int clock[3600];
-int chk[3600], chk2[3600];
 int main(){
 	int h, m, s, dep, arr;
 	scanf("%d %d %d %d %d", &h, &m, &s, &dep, &arr);
 
-	clock[(h*300+m*60+6*s)%3600]=1;
-	clock[(m*60+s*6)%3600]=1;
-	clock[(s*60)%3600]=1;
-
-//	printf("%d %d %d\n", (h*30+m*6)%360, (m*6+s*6)%360, s*6%360);
-
-	for (int i=0; i<3600; i++) {
-		chk[i]=clock[i];
-		chk2[i]=clock[i];
+	if(dep>arr){
+		arr=arr^dep;
+		dep=arr^dep;
+		arr=arr^dep;
 	}
 
-	for (int i=dep*300; i<=(dep<arr?arr*300:arr*300+3600); i++){
-		chk[(i+1)%3600]|=chk[i%3600];
-	}
+	int ans=0;
+	if (h<arr && h>=dep) ans++;
+	if (m<arr*5 && m>=dep*5) ans++;
+	if (s<arr*5 && s>=dep*5) ans++;
 
-	for (int i=(dep>arr?dep*300:dep*300+3600); i>=arr*300; i--){
-		chk2[(i-1)%3600]|=chk2[i%3600];
-	}
-
-	arr=(arr*300)%3600;
-	if (chk[arr] && chk2[arr]) printf("NO\n");
-	else printf("YES\n");
+	if (ans==0 || ans==3) printf("YES\n");
+	else printf("NO\n");
 }
