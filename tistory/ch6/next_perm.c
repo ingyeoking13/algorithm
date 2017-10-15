4ca56cd876e6cee7f19eebacedf1c20865c8cc83
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2017-10-15 18:55:32 +0000

    next_perm

---
+++ tistory/ch6/next_perm.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int a[7] = {7, 6, 3, 5, 4, 2, 1};
+void swap(int* a, int* b){ *a^=*b; *b^=*a; *a^=*b; }
+
+int main(){
+
+	int i=6;
+	while (a>0 && a[i-1] > a[i]) i--;
+	if (i==0) { printf("Invalid\n"); return -1;}
+	int j=6;
+	while (a[i-1] >a[j]) j--;
+	swap(&a[i-1], &a[j]);
+	j=6;
+	while(i<j){
+		swap(&a[i], &a[j]);
+		i++;
+		j--;
+	}
+
+	for (int i=0; i<7; i++) printf("%d ", a[i]);
+	printf("\n");
+}
