50b9f9a87cf3635b971022a1dd93e6431987701d
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2017-10-24 01:11:26 +0000

    asdf

--- ncafe/369426.c
+++ ncafe/369426.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-int a[20][20];
+int a[20][20], p[20];
+void swap(int x, int y) { p[x]^=p[y], p[y]^=p[x], p[x]^=p[y]; }
+
+int perm(int n){
+	int i=n-1;
+	while(i>0 && p[i-1]>p[i]) i--;
+	if (i==0) return 0;
+	int j=n-1;
+	while(p[i-1]>p[j]) j--;
+	swap(i-1, j);
+	j=n-1;
+	while(i<j){
+		swap(i, j);
+		j--;
+		i++;
+	}
+	return 1;
+}
 int main(){
-
-	int n;
-	scanf("%d", &n);
-
+	int n; scanf("%d", &n);
 	//init
 	for (int i=0; i<n; i++){
 		for (int j=0; j<=i; j++){
@@ -13,13 +27,19 @@ int main(){
 			a[j][i]=a[i][j];
 		}
 	}
-	printf("\n");
+	for (int i=0; i<n; i++) p[i]=i;
 
-	//printf
-	for (int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
-			printf("%d ", a[i][j]);
+	//find answer
+	int ans=1<<20;
+	do{
+		if(p[0]==0){
+			int sum=0;
+			for (int i=1; i<n; i++){
+				sum+=a[p[i-1]][p[i]];
+			}
+			sum+=a[p[n-1]][p[0]];
+			if (sum<ans) ans=sum;
 		}
-		printf("\n");
-	}
+	}while(perm(n));
+	printf("%d\n", ans);
 }
