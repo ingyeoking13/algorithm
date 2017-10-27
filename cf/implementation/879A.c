7a065a12f876f2f400e12735643c39a5d6bb005d
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2017-10-27 01:48:05 +0000

    asdf

--- cf/implementation/879A.c
+++ cf/implementation/879A.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int a[1000], b[1000];
+int a, b;
 int main(){
 
 	int n, t=0; scanf("%d", &n);
 	for (int i=0; i<n; i++){
-	 	scanf("%d %d", &a[i], &b[i]);
-		if (t<a[i]) t=a[i];
+	 	scanf("%d %d", &a, &b);
+		if (t<a) t=a;
 		else{
-			while(t>=a[i]) a[i]+=b[i];
-			t=a[i];
+			while(t>=a) a+=b;
+			t=a;
 		}
 	}
 	printf("%d\n", t);
