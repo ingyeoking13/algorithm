c566b72ecbdc31a25d8d1df22fa20f2624d9cf25
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2018-02-08 08:45:32 +0000

    asdf

--- tistory/tree/heapsort.c
+++ tistory/tree/heapsort.c
@@ -4,32 +4,32 @@ int a[21];
 void swap(int x, int y){ a[x]^=a[y], a[x]^=a[y], a[x]^=a[y]; }
 
 void heapify(int size, int n){
-    int max = n;
-    if ( n*2 <= size && a[max] < a[n<<1])  max=n<<1;
-    if ( n*2+1 <= size && a[max] < a[(n<<1)+1]) max = (n<<1)+1;
-    if ( max != n){
-				swap(n, max);
-        heapify(size, max);
-    }
+	int max = n;
+	if ( n*2 <= size && a[max] < a[n<<1])  max=n<<1;
+	if ( n*2+1 <= size && a[max] < a[(n<<1)+1]) max = (n<<1)+1;
+	if ( max != n){
+		swap(n, max);
+		heapify(size, max);
+	}
 }
 
 void heapsort(int size){
-    for (int i=size/2; i>=1; i-- ) heapify(size,i); //build heap
+	for (int i=size/2; i>=1; i-- ) heapify(size,i); //build heap
 
-    for (int i=size; i>=2; i--){
-				swap(i, 1);
-        size--;
-        heapify(size, 1);
-    }
+	for (int i=size; i>=2; i--){
+		swap(i, 1);
+		size--;
+		heapify(size, 1);
+	}
 }
 
 int main(){
-    for (int i=1; i<=20; i++) a[i]= 20-i;
-    for (int i=1; i<=20; i++) printf("%d ", a[i]);
-    printf("\n");
+	for (int i=1; i<=20; i++) a[i]= 20-i;
+	for (int i=1; i<=20; i++) printf("%d ", a[i]);
+	printf("\n");
 
-    heapsort(20);
+	heapsort(20);
 
-    for (int i=1; i<=20; i++) printf("%d ", a[i]);
-    printf("\n");
+	for (int i=1; i<=20; i++) printf("%d ", a[i]);
+	printf("\n");
 }
