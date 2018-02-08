4befb908b03cf0a50970260a423818b9ed82b6df
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2018-02-08 08:44:21 +0000

    asdf

--- tistory/tree/heapsort.c
+++ tistory/tree/heapsort.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int a[21];
+void swap(int x, int y){ a[x]^=a[y], a[x]^=a[y], a[x]^=a[y]; }
+
 void heapify(int size, int n){
     int max = n;
     if ( n*2 <= size && a[max] < a[n<<1])  max=n<<1;
     if ( n*2+1 <= size && a[max] < a[(n<<1)+1]) max = (n<<1)+1;
     if ( max != n){
-        a[n]^=a[max], a[max]^=a[n], a[n]^=a[max];
+				swap(n, max);
         heapify(size, max);
     }
 }
@@ -15,7 +17,7 @@ void heapsort(int size){
     for (int i=size/2; i>=1; i-- ) heapify(size,i); //build heap
 
     for (int i=size; i>=2; i--){
-        a[i]^=a[1], a[1]^=a[i], a[i]^=a[1];
+				swap(i, 1);
         size--;
         heapify(size, 1);
     }
