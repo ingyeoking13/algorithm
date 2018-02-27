db76f893e27644a96c058976d495d9d6edb87789
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2018-02-27 09:16:10 +0000

    asdf

--- bj/p1000/11657.cc
+++ bj/p1000/11657.cc
@@ -18,7 +18,6 @@ int main(){
 
 	for (int i=1; i<=n-1; i++){
 
-		for (int m=2; m<=n; m++) printf("d[%d]: %d\n", m, d[m]==1<<20?-1:d[m]);
 		for (int j=1; j<=n; j++){
 			for (int k=0; k<e[j].size(); k++){
 				if (d[e[j][k].v] > d[j] + e[j][k].w){
