250a93472155fecba8eb99e16b43f44845bd01d4
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2018-04-17 05:13:46 +0000

    asdf

--- kmong/job7_changedecimal/main.c
+++ kmong/job7_changedecimal/main.c
@@ -6,14 +6,14 @@ int getInput(int);
 typedef struct node{ char val; struct node* next; }Node;
 typedef struct{ struct node* top; }Stack;
 
-Stack* Init(){
+Stack* Init(){ // stack init
 	Stack* s= (Stack*) malloc(sizeof(Stack));
 	s->top=0;
 	return s;
 }
 
-char prec(int n ){
-	if (n >=10) return n-10+'A';
+char prec(int n ){ // precalculation for stack push
+	if (n >=10) return n-10+'A'; // we can use alphabet for 11~16 base.
 	else return n+'0';
 }
 
@@ -25,20 +25,21 @@ void push(Stack* s, int n){
 	s->top = newN;
 }
 
-void pop(Stack* s){
+void pop(Stack* s){ // stack pop
 	Node* now = s->top;
 	s->top= s->top->next;
 	free(now);
 }
 
-char top(Stack* s){ return s->top->val; } 
-int empty(Stack* s){ return s->top==0; }
+char top(Stack* s){ return s->top->val; }  // stack top
+int empty(Stack* s){ return s->top==0; } // stack empty
 
 int main(){
 
 	int  n=0;
 	Stack* s=Init();
-	while(1){
+	while(1){ // menu
+
 		printf("1. Insert a decimal number\n");
 		printf("2. Convert\n");
 		printf("3. Exit\n");
@@ -46,24 +47,22 @@ int main(){
 
 		if ( ans== 1) n= getInput(ans);
 		else if (ans==2 ){
-			int base = getInput(ans);
+			int base = getInput(ans); // we get base
 			int tmp=n;
 
-			while(tmp){
-				push(s, tmp%base);
+			while(tmp){  // while number modular
+				push(s, tmp%base); // we push it 
 				tmp/=base;
 			}
-			while(!empty(s)){
-				printf("%c", top(s));
+
+			while(!empty(s)){  // and then we pop it from the top of stack
+				printf("%c", top(s)); // while top exist
 				pop(s);
 			}
 			printf("\n");
 
 		}
-		else if (ans==3){
-			printf("Exit\n");
-			break;
-		}
+		else if (ans==3){ printf("Exit\n"); break; }
 		else getInput(ans);
 	}
 }
