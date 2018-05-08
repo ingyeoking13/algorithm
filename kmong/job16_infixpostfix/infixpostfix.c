2e122f9e718d796d509ae1a854092ca43eb72a0d
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2018-05-08 14:42:01 +0000

    asdf

--- kmong/job16_infixpostfix/infixpostfix.c
+++ kmong/job16_infixpostfix/infixpostfix.c
@@ -14,7 +14,8 @@
 	 여기서 이제 다음 character가 ')' 인데 
 	 괄호연산자는 연산의 우선순위를 정하므로, 
 	 여기서 한번 '(' 까지의 stack 자료를 다 뱉어냄으로써
-	 postfix에서의 연산 우선순위를 지킬 수있다.
+	 postfix에서의 연산 우선순위를 지킬 수있다. 
+	 괄호 뒤의 수식은 괄호 앞의 연산보다 낮은걸 생각하자.
 
 	 ret B.+.A
 
@@ -28,6 +29,18 @@
 
 	 지금 따라온 것은 정확하게 수행하면 항상 valid 한 infix를 valid한 postfix 수식으로 바군다.
 	 이런 순차적인 행위를 알고리즘이라 하며, infix -> postfix 로 바꾸는 알고리즘이다.
+
+	 괄호가 연산의 우선순위를 결정하고 그게 postfix로 변환하는데 중요한 역할을 하는것을 
+	 다시 유념하자, 괄호가 나오면 해당 여는 괄호까지 모든 stack을 뱉어낸다.
+
+	 하지만 연산의 우선순위는 +, *, -, / 사이에도 존재한다.
+	 만약 당신이 임의의 연산자를 받았다고 하자. 만약 그 연산자 뒤로 오는것이 앞에 오는것보다
+	 우선순위 가 낮다면 그 앞에것들의 스택을 뱉어내야한다.
+
+	 어떤 연산자가 주어지면 그 뒤의 식이 그 앞에것보다 우선순위가 낮을까?
+	 바로 '+', '-' 이다.
+	 A+B-C이면, AB+C- 가 되어야한다. ABC-+ 와 는 다르다는것을 명심하자.
+	 따라서, '+', '-' 연산자를 받으면 stack이 연산자일 때까지 다 뱉어내면된다.
 
 	 조금 더 전문적으로 기술하면, 사실 모든 수식은 tree형태로 그려질 수 있다. 
 	 (이미 수업시간에 들었을 수도 있지만...)
@@ -45,7 +58,6 @@
 
 	 그리고 이 tree 탐색을 stack을 이용해서 할 수 있는것이다. 그 성격이 같은 재귀로해도된다. 
 
-
 	 ps. 만약 당신이 어떠한 수에도 잘 동작하려고 한다면, 조금 더 복잡한 방법을 따라야한다.
 	 왜냐하면 char 형 stack은 오직 1byte만 담을수 있기 때문이다.
 	 만약 당신이 여러 바이트의 숫자를 다루려면 (즉, 2자리 이상의...)
@@ -58,8 +70,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-char s[10000]; //대충 stack size 10000...
-int top =-1;
+int s[10000], top =-1; //대충 stack size 10000...
 int oper(char c){ return c =='+' || c=='-' || c=='*' || c=='/'; }
 
 char* infix_to_postfix(char* str){
@@ -70,12 +81,17 @@ char* infix_to_postfix(char* str){
 	for (int i=0; i<len; i++){ // S문자열 을 순회!
 		int chk=0;
 		if (oper(str[i])) {
-			if(str[i]=='*' || str[i]=='/') s[++top] = str[i]; //S[i] 가 연산자일경우
-			else{
+			if(str[i]=='*' || str[i]=='/') {
+				while(top>=0 && (s[top]=='*' || s[top]=='/')){
+					ret[retlen++] = s[top--];
+				}
+				s[++top] = str[i]; //S[i] 가 연산자일경우
+			}
+			else{ // s[i] == '+', s[i] =='-' 이면, 앞의 연산자일 때까지 다 뱉어낸다.
 				while(top>=0 && oper(s[top])){
 					ret[retlen++] = s[top--]; 
 				}
-				s[++top] =str[i];
+				s[++top] =str[i]; // 그리고 해당녀석은stack에 들어감 
 			}
 		}
 		else if(str[i] == '(') s[++top] = str[i]; //S[i] 가 괄호일경우
@@ -85,7 +101,7 @@ char* infix_to_postfix(char* str){
 			}
 			top--; //그리고 한번더 top을뺀다 (이건 여는괄호이다)
 		}
-		else ret[retlen++]=str[i]; //숫자나 뭐 다른 기호일경우 뺀다
+		else ret[retlen++]=str[i]; //숫자인 경우
 	}
 
 	while(top>=0) ret[retlen++]=s[top--];
@@ -98,7 +114,7 @@ int evalPostfix(char* str){
 
 	for (int i=0; i<len; i++){
 		if (oper(str[i])){
-			int a=s[top--]; 
+			int a=s[top--];
 			int b=s[top--];
 			if (str[i] == '-') s[++top] = b-a;
 			else if (str[i]=='+') s[++top] =b+a;
@@ -113,7 +129,8 @@ int evalPostfix(char* str){
 
 int main(){ // from the script
 //	char* infix = "(9-((4/2)+1))*((5*2)-1)";
-	char* infix = "9-3*2+5";
+//	char* infix = "9-3*2+5";
+	char* infix = "9*9/1*1*5*3/3";
 	printf("중위 표기식: %s\n", infix);
 
 	char* postfix = infix_to_postfix(infix);
