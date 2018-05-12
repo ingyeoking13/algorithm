61b731a925ddd43a187825e25f296264a9aa15b7
Yo Han <adkb@ip-172-31-8-247.ap-northeast-2.compute.internal>
2018-05-12 05:33:55 +0000

    asdf

--- kmong/job24_shortestpath/main.cc
+++ kmong/job24_shortestpath/main.cc
@@ -53,22 +53,20 @@ int main(){
 
 		int sz = e[now].size();
 		for (int i=0; i<sz; i++){
-			if ( d[e[now][i].v] >  cost + e[now][i].w){
-				d[e[now][i].v]  = cost + e[now][i].w;
-				
-				p[e[now][i].v].clear();
+			if ( d[e[now][i].v] > cost + e[now][i].w){
 
+				p[e[now][i].v].clear();
+				d[e[now][i].v] = cost + e[now][i].w;
 				p[e[now][i].v].push_back({now, i});  // we save it path
 				pq.push({-d[e[now][i].v], e[now][i].v});
 			}
-			else if ( d[e[now][i].v] == cost + e[now][i].v ){
-				p[e[now][i].v].push_back({now,i});
+			if ( d[e[now][i].v] == cost + e[now][i].w ){
+				p[e[now][i].v].push_back({now, i});  // we save it path
 			}
 		}
 	} // we retrieve shortest path and its length
 
-
-	dfs(t );
+	dfs(t);
 
 	fill(d.begin(), d.end(), 0x3f3f3f3f); // make each d[i] 1 billion...<=>  infinity
 
@@ -85,7 +83,7 @@ int main(){
 		int sz = e[now].size();
 		for (int i=0; i<sz; i++){
 			if ( e[now][i].w < 0 ) continue; // ignore if 
-			if ( d[e[now][i].v] >  cost + e[now][i].w){
+			if ( d[e[now][i].v] > cost + e[now][i].w){
 				d[e[now][i].v]  = cost + e[now][i].w;
 				pq.push({-d[e[now][i].v], e[now][i].v});
 			}
