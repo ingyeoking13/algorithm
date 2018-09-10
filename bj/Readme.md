74d1a7184c51c4a0c4f221a7e59ce88bf2a7ecdd
Jeong Yo han <ingyeoking13@gmail.com>
2018-09-10 21:07:39 +0900

    README added

---
+++ bj/Readme.md
@@ -0,0 +1,5 @@
+# [Baekjoon Online Judge](https://www.acmicpc.net/)
+
+**ID** : gaelim
+
+**member of** C언어를배우자, Road to Grandmaster
---
+++ bj/p1000/n2028.cs
@@ -0,0 +1,12 @@
+using System.IO;
+
+namespace M
+{
+  class n2028
+  {
+    static public void Main()
+    {
+      int n =int.Parse(Console.ReadLine());
+    }
+  }
+}
