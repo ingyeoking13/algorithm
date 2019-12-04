9d7ed80db8e2a7f1e42fdac0103661944f7eed9e
ingyeoking13 <ingyeoking13@gmail.com>
2019-12-04 23:47:20 +0900

    ...

--- bj/p1000/1933.cc
+++ bj/p1000/1933.cc
@@ -1,67 +1,78 @@
 #include <bits/stdc++.h>
-using ll = long long;
 
 using namespace std;
 
-struct node
+struct Input 
 {
     int x, y, h;
-    node( int x, int y, int h ) : x(x), y(y), h(h) {};
-    node() {};
-    bool operator< (node o)
+    Input(int x, int y, int h) : x(x), y(y), h(h) {};
+    Input(){};
+    bool operator<(const Input& o ) const
     {
-        return  x< o.x;
+        return x < o.x;
     }
 };
 
+Input input[(int)1e5];
+int field[(int)2e5];
+
+priority_queue<Input> pq, pq2;
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
     int n;
     cin >> n;
-
-    node a[(int)1e5];
     for (int i=0; i<n; i++)
     {
-        cin >> a[i].x >> a[i].y >> a[i].h;
+        cin >> input[i].x >> input[i].h >> input[i].y;
+        input[i].x = -input[i].x;
+        input[i].y = -input[i].y;
+        pq.push(input[i]);
     }
 
-    sort(a, a+n);
-
-    priority_queue<pair<int, int>> pq;
-    map<int, int> ans;
-
-    int mxid = -1;
-    for (int i=0; i<n; i++)
+    int cur = 0, latest=0;
+    multiset<int> mm;
+    while(!pq.empty() || !pq2.empty())
     {
-        if (pq.size() == 0)
-        {
-            ans[a[i].x] = a[i].h;
-        }
-        else 
-        {
-            pair<int,int> cur = pq.top();
-            int y = -cur.first;
 
-            if ( y <= a[i].y)
+        while ( !pq.empty() && (pq2.empty() || -pq2.top().x > -pq.top().x))
+        {
+            Input now_meet_Building = pq.top();
+            pq.pop();
+            if ( cur < now_meet_Building.h )
             {
-                if ( mxid == cur.second) 
-                {
-                    mxid = 
+                cur = now_meet_Building.h;
+                cout << -now_meet_Building.x << " " << cur << " ";
+            }
+            mm.insert(-now_meet_Building.h);
+            pq2.push({now_meet_Building.y,0, now_meet_Building.h} );
+        }
 
-                }
-                pq.pop();
+        Input ended_building = pq2.top();
+        pq2.pop();
+
+        mm.erase(-ended_building.h);
+
+
+        int tmp; 
+        if ( mm.size() ==0) tmp = 0;
+        else tmp = *mm.begin();
+        tmp = -tmp;
+
+        if ( ended_building.h == cur && tmp < cur )
+        {
+            if ( !pq.empty() && pq.top().x == ended_building.x && pq.top().h >= ended_building.h)
+            {
+
             }
             else 
             {
+                if ( latest != -ended_building.x)
+                     cout << -ended_building.x << " " << tmp << " ";
+
+                cur = tmp;
+                latest = -ended_building.x;
             }
-
-
-
         }
-
     }
 }
