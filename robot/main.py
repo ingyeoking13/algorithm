9f13ec9fe8ad7d1a8c8d43f5d1a266d393624332
Jeong Yo Han <ingyeoking13@gmail.com>
2022-08-06 00:56:09 +0900

    robot 알고리즘 2d list 서 tuple로 변경

--- robot/main.py
+++ robot/main.py
@@ -2,60 +2,63 @@ from copy import deepcopy
 import functools
 from pydantic import BaseModel
 from queue import PriorityQueue, Queue 
-from typing import List, Optional, Any
+from typing import  Any, cast, List, Optional, Tuple
 import time
-source = [[ '1', '2', '3'],
-          ['4', '0', '6'], 
-          ['7','5','8']
-          ]
-target = [[ '1', '2', '3'],
-          ['4', '5', '0'],
-          ['6', '7', '8'] ]
-# source = [[ '0', '9', '7', '0', 'H'], 
-#           ['4', '13', '1', '10', '5'],
-#           ['11', '2', 'H', '0', '8'],
-#           ['6', '0', '0', '12', '3']]
 
-# target = [[ '1', '0', '3', '10', 'H'],
-#           ['13', '8', '0', '4', '12' ],
-#           ['6', '0', 'H', '7', '0'],
-#           ['11', '5', '0', '9', '2' ]]
+source = [[ '0', '9', '7', '0', 'H'], 
+          ['4', '13', '1', '10', '5'],
+          ['11', '2', 'H', '0', '8'],
+          ['6', '0', '0', '12', '3']]
+
+target = [[ '1', '0', '3', '10', 'H'],
+          ['13', '8', '0', '4', '12' ],
+          ['6', '0', 'H', '7', '0'],
+          ['11', '5', '0', '9', '2' ]]
+# source = [[ '1', '2', '3'],
+#           ['4', '0', '6'], 
+#           ['7','5','8']
+#           ]
+# target = [[ '1', '2', '3'],
+#           ['4', '5', '0'],
+#           ['6', '7', '8'] ]
+
+# source = [[ '1', '2', '3'],
+#           ['4', '0', '5'] ]
+# target = [[ '1', '2', '3'],
+#           ['4', '5', '0']]
+# source = [[ '1', '0']]
+# target = [[ '0', '1' ]]
+
+tupled_target = tuple(char for row in target for char in row)
+
 
 @functools.total_ordering
 class Node:
-    state:List[List[str]]
-    px:int
-    py:int
+    state:Tuple[str]
+    p:int
     grab:str
     distance:int
 
     def ManhattanDistance(self):
         result = 0
-        row = len(self.state)
-        col = len(self.state[0])
+        col = len(source[0])
+        for i in range(len(self.state)):
+            elem = self.state[i]
+            if elem == '0': 
+                continue
 
-        for i in range(row):
-            for j in range(col):
-                elem = self.state[i][j]
-                if elem == '0': 
-                    continue
-                # Variable to break the outer loop and avoid unnecessary processing
-                found = False
-                # Loop to find element in goal state and MD
-                for k in range(row):
-                    for l in range(col):
-                        if target[k][l] == elem:
-                            result += abs(k - i) + abs(j - l)
-                            found = True
-                            break
-                    if found: break
+            for j in range(len(self.state)):
+                if tupled_target[j] == elem:
+                    diff = abs(j-i)
+                    diff = int(diff/col) + diff%col 
+                    result += diff
+                    break
 
         return result
 
-    def __init__(self, state, px, py, grab, distance) -> None:
+    def __init__(self, state:Tuple[str], p:int, grab:str, distance:int) -> None:
         self.state = state
-        self.px = px
-        self.py = py
+        self.p = p
         self.grab = grab
         self.distance = distance
 
@@ -70,78 +73,90 @@ class Solver(BaseModel):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     queue: Optional[Any]
+    solved_state: Optional[str]
+    visited:Optional[Any]
 
     def __init__(self, **data) -> None:
         super().__init__(**data)
     
-    def stringifyState(self, state:List[List[str]], px:int, py:int):
-        state_str = ','.join([ char for  row in  state for char in row ])
-        state_str += ',' + str(px) + ',' + str(py)
-        return state_str
-
     def solve(self, source: List[List[str]], target: List[List[str]]):
         rows = len(source)
         cols = len(source[0])
-        px = 0
-        py = 0
+        p = 0
 
-        state = deepcopy(source)
-        state_str = self.stringifyState(state, px, py)
-        target_str = self.stringifyState(target, 0, 0)
-        queue = self.queue
-        # queue  = Queue()
-        visited = set()
-        visited.add(state_str)
-        queue.put(Node(state, px,py, '', 0))
+        source_state = tuple(char for row in source for char in row)
+        tupled_target = tuple(char for row in target for char in row)
+
+        source_state_merged = source_state + (p,)
+        answer = tupled_target + (p,)
+
+        queue = cast(Queue, self.queue)
+        visited = self.visited = {}
+        visited[source_state_merged] = ''
+        queue.put(Node(source_state, p, '', 0))
 
         while not queue.empty():
             node: Node= queue.get()
-            state, px, py, grab, distance = (node.state , node.px, node.py, node.grab, node.distance)
-            # print(_state, px, py, distance)
-            next_state = deepcopy(state)
+            state, p, grab, distance = (node.state , node.p, node.grab, node.distance)
+            # print(state, p, grab)
+
             # put! 
-            if grab and next_state[px][py] == '0':
-                next_state[px][py] = grab
-                grab = ''
-                state_str = self.stringifyState(next_state, px, py)
-                if state_str not in visited:
-                    visited.add(state_str)
-                    if target_str.split(',')[:-2] == state_str.split(',')[:-2]: return distance+1
-                    queue.put(Node(deepcopy(next_state), px, py, grab, distance+1))
-                grab = next_state[px][py]
-                next_state[px][py] = '0'
+            if grab and state[p] == '0': 
+                next_state = state[:p] + (grab,) + state[p+1:]
+                state_merged = next_state + (p,)
+                if state_merged not in visited:
+                    visited[state_merged] = {'map': state, 'grab': grab, 'p': p }
+
+                    # we solve!
+                    if answer[:-1] == state_merged[:-1]: 
+                        self.solved_state = state_merged
+                        return distance+1
+
+                    queue.put(Node(next_state, p, '', distance+1))
+
 
             # grab!
-            if not grab and next_state[px][py] != '0':
-                grab = next_state[px][py]
-                next_state[px][py] = '0'
-                state_str = self.stringifyState(next_state, px, py)
-                if state_str not in visited:
-                    visited.add(state_str)
-                    queue.put(Node(deepcopy(next_state), px, py, grab, distance+1))
-                next_state[px][py] = grab
-                grab = ''
+            if not grab and state[p] != '0':
+                next_state = state[:p] +  ('0',) + state[p+1:]
+                state_merged = next_state  +(p,)
+                if state_merged not in visited:
+                    visited[state_merged] = {'map': state, 'grab': grab, 'p':p}
+                    queue.put(Node(next_state, p, state[p], distance+1))
 
             # or... just move!
             for dir in range(4):
-                next_px = px + self.dx[dir]
-                next_py = py + self.dy[dir]
+                next_px = int(p/cols) + self.dx[dir]
+                next_py = p%cols + self.dy[dir]
+
                 if next_px < 0 or next_py < 0  or next_px >= rows or next_py >= cols: 
                     continue
-                if grab: 
-                    if next_state[next_px][next_py] != '0':
-                        continue
+
+                next_p = next_px*cols + next_py
+                if grab and state[next_p] != '0':
+                    continue
                     
-                state_str = state_str = self.stringifyState(next_state, next_px, next_py)
-                if state_str not in visited:
-                    visited.add(state_str)
-                    queue.put(Node(deepcopy(next_state), next_px, next_py, grab, distance+1))
+                state_merged =  state + (next_p,)
+                if state_merged not in visited:
+                    visited[state_merged] = {'map': state, 'grab': grab, 'p': p}
+                    queue.put(Node(state, next_p, grab, distance+1))
 
         return -1
 
-solver = Solver(queue=Queue())
+    def printPath(self):
+        # print(self.visited)
+        pass
+
+
+solver = Solver(queue=PriorityQueue())
+# solver = Solver(queue=Queue())
 
 st = time.time()
-print(solver.solve(source, target))
+result = solver.solve(source, target)
+print('solved: ', result, ' with times operator') if result >= 0 else print('unsolved')
+
+if result > 0:
+    solver.printPath()
+
 end = time.time()
+
 print(end- st)
